3500a3ab67997b2265a65a6c1527e108070da312
Stephen-C-Noh <stephen.c.noh@gmail.com>
2022-02-23 21:42:50 +0000

    login failing

---
+++ accounts/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+from django.contrib.auth.models import User
+
+
+class SignupForm(UserCreationForm):
+    username = forms.CharField(label='ID for Login', widget=forms.TextInput(attrs={
+        'pattern': '[a-zA-Z0-9]+',
+        'title': 'Special Characters and Blanks are not allowed',
+    }))
+    
+    nickname = forms.CharField(label='Nick Name')
+    picture = forms.ImageField(label='Profile Picture', required=False)
+    
+    class Meta(UserCreationForm.Meta):
+        fields = UserCreationForm.Meta.fields + ('email',)
+        
+def clean_nickname(self):
+    nickname = self.cleaned_data.get('nickname')
+    if Profile.objects.filter(nickname=nickname).exist():
+        raise forms.ValidationError('This nickname is already in use.')
+    return nickname
+
+def clean_email(self):
+    email = self.cleaned_data.get('email')
+    User = get_user_model()
+    if User.objects.filter(email=email).exist():
+        raise forms.ValidationError('This email is already in use.')
+    return email
+
+def clean_picture(self):
+    picture = self.cleaned_data.get('picture')
+    if not picture:
+        picture = None
+    return picture
+
+def save(self):
+    user = super().save()
+    Profile.objects.create(
+        user=user,
+        nickname = self.cleaned_data['nickname'],
+        picture=self.cleaned_data['picture'],
+    )
+    return user
+
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'config' / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instaclone',
+        'USER': 'stephen',
+        'PASSWORD': 'shdwkd123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -105,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
@@ -118,3 +125,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'config' / 'static',
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGIN_REDIRECT_URL = '/'
