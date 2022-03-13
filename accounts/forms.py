fbb28e30c29f5c2c09d5ce02a47cee8cae710f6f
Stephen Changbeom Noh <97701435+Stephen-C-Noh@users.noreply.github.com>
2022-03-12 23:14:30 -0500

    Profile auto-generates upon signup. css touch ups.

--- accounts/forms.py
+++ accounts/forms.py
@@ -16,38 +16,38 @@ class SignupForm(UserCreationForm):
         'pattern': '[a-zA-Z0-9]+',
         'title': 'Special Characters and Blanks are not allowed',
     }))
-    
+
     nickname = forms.CharField(label='Nick Name')
     picture = forms.ImageField(label='Profile Picture', required=False)
-    
+
     class Meta(UserCreationForm.Meta):
         fields = UserCreationForm.Meta.fields + ('email',)
-        
-def clean_nickname(self):
-    nickname = self.cleaned_data.get('nickname')
-    if Profile.objects.filter(nickname=nickname).exist():
-        raise forms.ValidationError('This nickname is already in use.')
-    return nickname
 
-def clean_email(self):
-    email = self.cleaned_data.get('email')
-    User = get_user_model()
-    if User.objects.filter(email=email).exist():
-        raise forms.ValidationError('This email is already in use.')
-    return email
+    def clean_nickname(self):
+        nickname = self.cleaned_data.get('nickname')
+        if Profile.objects.filter(nickname=nickname).exists():
+            raise forms.ValidationError('This nickname is already in use.')
+        return nickname
 
-def clean_picture(self):
-    picture = self.cleaned_data.get('picture')
-    if not picture:
-        picture = None
-    return picture
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        User = get_user_model()
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('This email is already in use.')
+        return email
 
-def save(self):
-    user = super().save()
-    Profile.objects.create(
-        user=user,
-        nickname = self.cleaned_data['nickname'],
-        picture=self.cleaned_data['picture'],
-    )
-    return user
+    def clean_picture(self):
+        picture = self.cleaned_data.get('picture')
+        if not picture:
+            picture = None
+        return picture
+
+    def save(self):
+        user = super().save()
+        Profile.objects.create(
+            user=user,
+            nickname = self.cleaned_data['nickname'],
+            picture=self.cleaned_data['picture'],
+        )
+        return user
 
